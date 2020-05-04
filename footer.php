11c69d3a5bc1e8b129da741103617cb00a4f1ab0
Martin Davis <martin_davis3@yahoo.ca>
2020-05-04 16:37:28 -0600

    completed theme

--- footer.php
+++ footer.php
@@ -5,6 +5,11 @@
       <?php echo date( 'Y' ); ?>
       Martin Davis
     </p>
+    <?php if ( function_exists( 'output_hello_world' ) ) : // Check if the plugin function exists. ?>
+      <aside>
+        <?php output_hello_world(); ?>
+      </aside>
+    <?php endif; ?>
   </footer>
   <?php wp_footer(); ?>
 </body>
